9964812556536e1b368f4326657917c1b6b88fd3
hsnygr <37368110+hsnygr@users.noreply.github.com>
2018-04-16 11:59:15 +0300

    Update ornek1Javaileilkprogrammimiz.java

--- ornek1Javaileilkprogrammimiz.java
+++ ornek1Javaileilkprogrammimiz.java
@@ -8,5 +8,6 @@ public static void main(String[] args) {
         System.out.print("Merhaba");
         System.out.print("Java");
         System.out.println("Dünyası");
+        //println dan sonra gelen ifade bir alt satıra yazılacaktır.
         System.out.println("aradaki farka dikkat edelim");
     }
