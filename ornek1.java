776f07725fa3407eb4c4e9d148b75755c35545a6
hsnygr <37368110+hsnygr@users.noreply.github.com>
2018-04-16 11:14:05 +0300

    Create ornek1.java

---
+++ ornek1.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+        
+        System.out.println("Merhaba Java");
+        System.out.println("MerhabaJava");
+        System.out.println("Merhaba"+"Java");
+        System.out.println("15"+15);
+        
+        System.out.print("Merhaba");
+        System.out.print("Java");
+        System.out.println("Dünyası");
+        System.out.println("aradaki farka dikkat edelim");
+    }
