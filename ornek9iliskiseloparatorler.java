5fe0835529937badf2f403bd95989c07089fcb27
Hasan Uygur <37368110+hsnygr@users.noreply.github.com>
2018-04-18 15:11:53 +0300

    Create ornek9iliskiseloparatorler.java

---
+++ ornek9iliskiseloparatorler.java
@@ -0,0 +1,19 @@
+public static void main(String[] args) {
+        
+       int x = 7 , y = 5;
+       
+       System.out.println(x<y);//x,y den küçük mü
+       
+       System.out.println(x + "<" + y + ":" + (x<y));
+       //yukarıdaki her iki ifadede matematiksel işlemi döndürür
+       //ancak ikincisi daha anlaşılır olmuştur.
+       
+       System.out.println();//boşluk bırakalım
+       System.out.println(x + ">" + y + ":" + (x>y));//x yden büyük mü
+       System.out.println(x + "<=" + y + ":" + (x<=y));//x yden küçük eşitmi
+       System.out.println(x + ">=" + y + ":" + (x>=y));//x yden büyük eşitmi
+       System.out.println(x + "==" + y + ":" + (x==y));//x yeye eşitmi
+       System.out.println(x + "!=" + y + ":" + (x!=y));//x yeye eşit değilmi
+       
+       
+    }
