4706e4a26a89dd2803a8c4ecbbbd7572bc8233a4
Hasan Uygur <37368110+hsnygr@users.noreply.github.com>
2018-04-21 09:22:58 +0300

    Create ornek23whileDongusu.java

---
+++ ornek23whileDongusu.java
@@ -0,0 +1,17 @@
+public static void main(String[] args) {
+        
+       int sayi = 5;
+       //doğru sınama değişkeni
+       while(sayi >= 0){
+           //dongü koşulu
+           System.out.println("5 kere dondureceğiz");
+           sayi--;
+       }
+       // 1 den 10 kadar sayilari ekrana bastıralım
+       int abakus = 0;
+       while(abakus<=10){
+           System.out.println("abakus =" + abakus);
+           abakus ++;
+       }
+       System.out.println("abakus doğru sayiyor:)");
+    }
