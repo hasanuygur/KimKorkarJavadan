2218c1d382b132f8f665ba8f328f308ea151a0cf
Hasan Uygur <37368110+hsnygr@users.noreply.github.com>
2018-04-21 17:43:40 +0300

    Create ornek25classForDongusu.java

---
+++ ornek25classForDongusu.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+        
+      for (int sayi = 0; sayi < 5; sayi++){
+          System.out.println("sayi =" + sayi);
+      } 
+      
+      for (int x = 0, y = 0;x < 5; x++ , y++){
+          x*=y;
+          System.out.println("x =" + x + " y =" + y);
+      }
+      
+    }
