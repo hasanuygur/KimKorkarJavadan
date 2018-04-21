ecdf01c25f3c51b63afc3d50a7ff07e75a4e29d4
Hasan Uygur <37368110+hsnygr@users.noreply.github.com>
2018-04-21 09:07:00 +0300

    Create ornek15KodBloklari.java

---
+++ ornek15KodBloklari.java
@@ -0,0 +1,34 @@
+public static void main(String[] args) {
+        
+        int seviye1Blok1 = 1;
+        
+        System.out.println("seviye1Blok1 :" + seviye1Blok1);
+        
+        {
+            int seviye2Blok1 = 21;
+            
+            
+            {
+                int seviye3Blok1 = 31;
+                
+                System.out.println("seviye1Blok1 :" + seviye1Blok1);
+                System.out.println("seviye2Blok1 :" + seviye2Blok1);
+                System.out.println("seviye3Blok1 :" + seviye3Blok1);
+            }
+            
+            {
+                int seviye3Blok2 = 32;
+            }
+            
+            {
+                int seviye3Blok3 = 33;
+                
+                System.out.println("seviye1Blok1 :" + seviye1Blok1);
+                System.out.println("seviye2Blok1 :" + seviye2Blok1);
+                
+            }
+            }
+            
+            System.out.println("seviye1Blok1 :" + seviye1Blok1);
+            }
+    } //ek1:hiç bişey anlamadım :)
