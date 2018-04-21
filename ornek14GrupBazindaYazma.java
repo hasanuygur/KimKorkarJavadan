0566e78ee7cd2a4602fd9775cb758be28d6d9ba3
Hasan Uygur <37368110+hsnygr@users.noreply.github.com>
2018-04-21 09:06:30 +0300

    Create ornek14GrupBazindaYazma.java

---
+++ ornek14GrupBazindaYazma.java
@@ -0,0 +1,24 @@
+public static void main(String[] args) {
+        
+        byte b = 0b0010_0101;
+        short s = (short) 0b1010_100101_001100;
+        
+        int odeme = 1_000_000;
+        
+        long krediKartiNumarasi = 1234_5678_9000_2012L;
+        long I = 0b11010010_01101001_10010100_10010010L;
+        
+        float piSayisi = 3.14_15F;
+        
+        double d = 0x2p5;
+        double d2 = 0x32p-8;
+        
+        System.out.println("b =" + b);
+        System.out.println("s =" + s);
+        System.out.println("odeme =" + odeme);
+        System.out.println("krediKartiNumarasi =" + krediKartiNumarasi);
+        System.out.println("I = " + I);
+        System.out.println("piSayisi =" + piSayisi);
+        System.out.println("d =" + d);
+        System.out.println("d2 =" + d2);
+    }
