6a40d495e1544eff6274aa83869c03b5e80984c1
Hasan Uygur <37368110+hsnygr@users.noreply.github.com>
2018-04-21 09:13:09 +0300

    Create ornek19ifElseYapisi.java

---
+++ ornek19ifElseYapisi.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) {
+        
+       int birinciSayi = 15;
+       int ikinciSayi = 25;
+       
+       if(birinciSayi==ikinciSayi){
+           System.out.println("birinciSayi ikinciSayiya eşittir");
+       }
+       else {
+           System.out.println("birinciSayi ikinciSayidan küçüktür.");
+       }
+        
+    }
