b92acc41f1a3ea45924f2313411c7a5cd15e02c9
Hasan Uygur <37368110+hsnygr@users.noreply.github.com>
2018-04-25 18:23:41 +0300

    Create ornek36Sayac.java

---
+++ ornek36Sayac.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+        
+        int sayac = 0;
+        do
+        {
+        System.out.println("ALPER");
+        sayac++;
+        }
+        While (sayac <5);
+        
+        }
