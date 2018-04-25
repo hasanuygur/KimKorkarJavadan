c63d6520dfb4f3cf3a944eaff11c83e38892d1bd
Hasan Uygur <37368110+hsnygr@users.noreply.github.com>
2018-04-25 18:14:07 +0300

    Create ornek32Algoritma.java

---
+++ ornek32Algoritma.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) {
+        
+        Scanner tara = new Scanner (System.in);
+        int x,y,toplam;
+        
+        System.out.println(" x sayisini giriniz.");
+        x = tara.nextInt();
+        System.out.println(" y sayisini giriniz.");
+        y = tara.nextInt();
+        
+        toplam = x + y;
+        System.out.println("toplam =" + toplam);
+        
+    }
