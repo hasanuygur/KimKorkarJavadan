98f22945c6f0b73755c0a9fa44a0cc5cd5ac0fe3
Hasan Uygur <37368110+hsnygr@users.noreply.github.com>
2018-04-25 18:34:52 +0300

    Create ornek38Abakus.java

---
+++ ornek38Abakus.java
@@ -0,0 +1,16 @@
+public static void main(String[] args) {
+        
+        for (int sayi1 = 1; sayi1 <= 100; sayi1++)
+        {
+            System.out.println("Sayi1 =" + sayi1);
+        }
+        
+        for (int sayi2 = 100; sayi2 >= 1; sayi2--)
+        {
+            System.out.println("Sayi2 =" + sayi2);
+        }
+        
+        
+        
+        
+        }
