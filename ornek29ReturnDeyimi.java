6a9f1cdfa0b020490cfaf81efd1f1811423c0803
Hasan Uygur <37368110+hsnygr@users.noreply.github.com>
2018-04-25 18:12:57 +0300

    Create ornek29ReturnDeyimi.java

---
+++ ornek29ReturnDeyimi.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+        for (int x= 0; x < 10; x++){
+            if(x==5){
+                return;
+            }
+            else {
+                System.out.println("x =" + x);
+            }
+        }
+        
+        
+    }
