4fd337d44caba4dc08262429582f71f0912fbf83
Hasan Uygur <37368110+hsnygr@users.noreply.github.com>
2018-04-25 18:14:39 +0300

    Create ornek33Ortalama.java

---
+++ ornek33Ortalama.java
@@ -0,0 +1,18 @@
+public static void main(String[] args) {
+        
+        Scanner tara = new Scanner(System.in);
+        int x,y,z,ortalama;
+        
+        System.out.println("x sayisiniz giriniz");
+        x = tara.nextInt();
+        
+        System.out.println("y sayisiniz giriniz");
+        y = tara.nextInt();
+        
+        System.out.println("z sayisiniz giriniz");
+        z = tara.nextInt();
+        
+        ortalama =(x+y+x)/3;
+        
+        System.out.println("ortalam =" + ortalama);
+        }
