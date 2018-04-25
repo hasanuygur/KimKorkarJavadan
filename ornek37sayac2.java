fb0c179f62c279f7d4d64c30b6bf0c365a208478
Hasan Uygur <37368110+hsnygr@users.noreply.github.com>
2018-04-25 18:24:20 +0300

    Create ornek37sayac2.java

---
+++ ornek37sayac2.java
@@ -0,0 +1,18 @@
+public static void main(String[] args) {
+        
+        int sayac = 1;
+        int toplam = 0;
+        
+        do
+        {
+            toplam = toplam + sayac;
+            sayac++;
+        }
+        
+        while(sayac <= 100);
+            System.out.println("toplam1-100 =" + toplam);
+        
+        
+        
+        
+        }
