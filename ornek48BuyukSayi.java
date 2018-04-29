d4c5a4d84c1b5c0f6eb9f202dbb596e902b72882
Hasan Uygur <37368110+hsnygr@users.noreply.github.com>
2018-04-29 17:05:17 +0300

    Create ornek48BuyukSayi.java

---
+++ ornek48BuyukSayi.java
@@ -0,0 +1,15 @@
+public static void main(String[] args) {
+        
+        
+        int x,y;
+        
+        x = (0 + (int) (Math.random()*50));
+        System.out.println("x =" + x);
+        
+        y = (0 + (int) (Math.random()*50));
+        System.out.println("y =" + y);
+        
+        System.out.println("büyük sayi :" + Math.max(x, y) +" " + "küçük sayi :" + Math.min(x, y));
+        
+        
+        }
