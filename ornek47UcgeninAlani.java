0c00b0617c9b82639698f02a506837d1845a00db
Hasan Uygur <37368110+hsnygr@users.noreply.github.com>
2018-04-29 16:56:36 +0300

    Create ornek47UcgeninAlani.java

---
+++ ornek47UcgeninAlani.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+        
+        
+        double x, Alan;
+        x = 5;
+        System.out.println("x kenar uzunluğu :" + x);
+        Alan = Math.pow(x, 2)/2;
+        System.out.println("Alan =" + Alan);
+        
+        
+        
+        }
