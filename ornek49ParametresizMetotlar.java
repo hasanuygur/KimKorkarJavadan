16c4ac3453648a70cc019fa0872579fa81bfbf77
Hasan Uygur <37368110+hsnygr@users.noreply.github.com>
2018-05-02 11:42:33 +0300

    Create ornek49ParametresizMetotlar.java

---
+++ ornek49ParametresizMetotlar.java
@@ -0,0 +1,9 @@
+static void parametresiz ()
+    {
+        System.out.println("parametresiz metotlar");
+    }
+    public static void main(String[] args) {
+        
+        parametresiz();
+        
+        }
