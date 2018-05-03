cca7bd26740f4952898071cd9ca712416aba309a
Hasan Uygur <37368110+hasanuygur@users.noreply.github.com>
2018-05-03 10:35:39 +0300

    Create ornek58Dizi.java

---
+++ ornek58Dizi.java
@@ -0,0 +1,9 @@
+public static void main(String[] args) {
+        
+        int[] yeniDizi = new int[10];
+        //burada yeniDizi isimli 10 elemanlı bir dizi tanımladık
+        int x;
+        for (x = 0; x < yeniDizi.length; x++){
+            System.out.println("indis :" + x + " değeri :" + yeniDizi[x]);
+        }
+    }
