9b5cbfe98d3bf5bf16fbaa59f6843d1f135568b4
Hasan Uygur <37368110+hasanuygur@users.noreply.github.com>
2018-05-03 10:13:10 +0300

    Create ornek55YapilandirmaciMetotlar.java

---
+++ ornek55YapilandirmaciMetotlar.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+        
+       for (int x=0;x<3;x++){
+           //varsayilan yapılandırıcı çağıralım çalıştıralım
+           YapilandiriciMetotlar y1 = new YapilandiriciMetotlar();
+       } 
+    }
+    //varsayilan yapılandırıcıyı oluşturalım
+    public static YapilandiriciMetotlar(){
+        System.out.println("ben bir yapilandirmaci metodum....");
+    }
