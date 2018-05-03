47cf354a374874ef7c008cd52b6f90b96a720b57
Hasan Uygur <37368110+hasanuygur@users.noreply.github.com>
2018-05-03 10:14:16 +0300

    Create ornek57DegiskenlerinFaaliyetAlanlari.java

---
+++ ornek57DegiskenlerinFaaliyetAlanlari.java
@@ -0,0 +1,12 @@
+    public static int farkiniAl(int x, int y){
+        int farkiniAl;
+        if (x > y){
+            farkiniAl = x - y;
+        }
+        else {
+            farkiniAl = y - x;
+        }
+        return farkiniAl;
+    }
+    
+    
