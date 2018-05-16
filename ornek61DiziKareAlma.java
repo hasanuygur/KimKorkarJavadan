edaeabec96d7f1ff64385bbaf9f11e3a754771c8
Hasan Uygur <37368110+hasanuygur@users.noreply.github.com>
2018-05-16 17:25:54 +0300

    Create ornek61DiziKareAlma.java

---
+++ ornek61DiziKareAlma.java
@@ -0,0 +1,9 @@
+public static void main(String[] args) {
+        
+       int[] kareDizi = new int[8];
+       for (int x=0; x<8; x++){
+           kareDizi[x]=x*x;
+           System.out.println("indis " + x + " degeri " + kareDizi[x]);
+       }
+        
+    }//public static void sonu
