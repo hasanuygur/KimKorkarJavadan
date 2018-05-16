9ed0156769ede51bcc40565822e2bbecdf0f1ef0
Hasan Uygur <37368110+hasanuygur@users.noreply.github.com>
2018-05-16 17:35:43 +0300

    Create ornek75Dizikarsilastirma.java

---
+++ ornek75Dizikarsilastirma.java
@@ -0,0 +1,6 @@
+public static void main(String[] args) {
+        int[] dizi1 = {3,8,5,7,6,4};
+        int[] dizi2 = {3,8,5,7,6,4};
+        System.out.println(Arrays.equals(dizi1, dizi2));
+        //dizilerin eşitliğikontrol edilir ve eşitse true değeri döndürülür.
+    }
