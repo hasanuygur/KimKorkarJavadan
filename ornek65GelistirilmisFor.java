c602f97471c98eae03cd552b633ab01c097f9291
Hasan Uygur <37368110+hasanuygur@users.noreply.github.com>
2018-05-16 17:27:32 +0300

    Create ornek65GelistirilmisFor.java

---
+++ ornek65GelistirilmisFor.java
@@ -0,0 +1,8 @@
+public static void main(String[] args) {
+        
+       int [] D = {1,2,3,4,5};
+       for (int Sayi : D){
+           //int parametremizin veri tipi , sayı da tanımlayıcıdır.
+           System.out.println("sayi :" + Sayi);
+       }
+    }
