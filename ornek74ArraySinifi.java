916370c726087306283a668ccdfe1d60919f59c5
Hasan Uygur <37368110+hasanuygur@users.noreply.github.com>
2018-05-16 17:35:10 +0300

    Create ornek74ArraySinifi.java

---
+++ ornek74ArraySinifi.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+        int dizi[] = {3,8,5,7,6,4};
+        Arrays.sort(dizi);
+        //dizileri sırlamak için array sınıfının sort metodu kullanılmaktadır.
+        for(int x=0; x<dizi.length; x++){
+            System.out.println(dizi[x]);
+        }
+        
+        
+    }
