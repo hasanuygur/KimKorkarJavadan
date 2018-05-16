cdd2db69ad5607a6f1c416e9820c6b4979177362
Hasan Uygur <37368110+hasanuygur@users.noreply.github.com>
2018-05-16 17:28:20 +0300

    Create ornek67MetotDiziEleman.java

---
+++ ornek67MetotDiziEleman.java
@@ -0,0 +1,14 @@
+public static void main(String[] args) {
+        
+       int[] dizi = {3,5,7,9,11};
+       //dizi isminde elemanları int tipinde bir dizi oluşturuldu.
+       
+       metodDizi(dizi[2]);
+       //oluşturulan metot dizinin 3. elemanı ile çağrıldı.
+       
+    }//public static void sonu
+    
+    public static void metodDizi(int x){
+        //metodDizi isimli bir metot oluşturuldu.
+        System.out.println(x);
+    }
