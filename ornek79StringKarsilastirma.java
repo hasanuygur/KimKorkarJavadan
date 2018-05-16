8e703b1fb0fac26bf57eeaf4d0cb2b9cf77dec72
Hasan Uygur <37368110+hasanuygur@users.noreply.github.com>
2018-05-16 17:38:43 +0300

    Create ornek79StringKarsilastirma.java

---
+++ ornek79StringKarsilastirma.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+       String takim = "Fenerbahçe";
+       String takim2 = "Fenerbahçe";
+       if (takim.equals(takim2)){
+           System.out.println("stringler eşittir");
+       }
+       else {
+           System.out.println("stringler eşit değildir.");
+       }
+
+    }
