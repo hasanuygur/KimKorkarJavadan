9465d768564d3d77f476e328872d2ae66d4674f7
Hasan Uygur <37368110+hasanuygur@users.noreply.github.com>
2018-05-16 17:25:16 +0300

    Update ornek60DiziEleman2.java

--- ornek60DiziEleman2.java
+++ ornek60DiziEleman2.java
@@ -1,1 +1,10 @@
-ornek60DiziEleman2
+public static void main(String[] args) {
+        
+       int[] yeniDizi = {10,20,30,40,50,60,70,80,90};
+       int x;
+       //yeniDizi.length ile dizinin boyutunun elemanları kadardır.
+            for(x=0; x<yeniDizi.length; x++){
+                System.out.println("indis " + x + " değeri " + yeniDizi[x]);
+            }
+        
+    }//public static void sonu
