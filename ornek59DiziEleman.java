919b2dd0d81d1c2063443136989e74888bf6ac9c
Hasan Uygur <37368110+hasanuygur@users.noreply.github.com>
2018-05-16 17:24:34 +0300

    Create ornek59DiziEleman.java

---
+++ ornek59DiziEleman.java
@@ -0,0 +1,18 @@
+public static void main(String[] args) {
+        
+       int[] yeniDizi = new int[10];
+       //dizimiz 10 adet eleman alabilir
+            //diziye gelişi güzel elemanlar atayalım
+            yeniDizi[0]=24;
+            yeniDizi[1]=34;
+            yeniDizi[5]=100;
+            yeniDizi[8]=-24;
+            yeniDizi[9]=-34;
+            
+            int x;
+            for(x = 0; x < yeniDizi.length; x++){
+                System.out.println("indis " + x + " değeri " + yeniDizi[x]);
+            }
+            
+        
+    }//public static void sonu
